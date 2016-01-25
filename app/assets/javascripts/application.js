5e087ede4c33c35d72a98b85e4bc2d0d74757450
Scott Ball <scotttball@gmail.com>
2016-01-25 15:02:52 -0700

    devise, react, materialize, g views for users & index

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+//= require react
+//= require react_ujs
+//= require materialize-sprockets
+//= require components
 //= require_tree .
