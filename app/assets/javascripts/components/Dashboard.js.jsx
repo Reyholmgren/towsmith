d07c784cdd81fd43eed9573236aaefbe3b164078
Rey Holmgren <rey.holmgren@gmail.com>
2016-01-25 17:25:53 -0700

    navbar and some index work

--- app/assets/javascripts/components/Dashboard.js.jsx
+++ app/assets/javascripts/components/Dashboard.js.jsx
@@ -1,36 +1,54 @@
 class Dashboard extends React.Component {
   constructor(props) {
     super(props);
+    this.newRequest=this.newRequest.bind(this);
   }
+
   componentWillMount(){
     $.ajax({
-      url: '/search',
+      url: '/requests',
       type: 'GET',
       dataType: 'JSON',
       data: { location: location }
     }).success( data => {
-      this.refs.search.value = null;
-      this.setState({ restaurants: data.restaurants });
+      this.setState({ requests: data });
     });
   }
+  newRequest(){
+    $.ajax({
+      url: '/request',
+      type: 'POST',
+      data: {request: {title: this.refs.newRequest.value}}
+    }).success( data => {
+      let requests = this.state.requests;
+      requests.push(data.request);
+      this.setState({requests: requests});
+    });
+  }
+
+
   render() {
+    let requests = this.state.requests.map(request => {
+      let key = `request-${request.title}`;
+      return( <Request key={key} {...request} />);
+    });
     return(
-        <div>
-          <h1 className='center'>TowSmith</h1>
-            <div className='container'>
+            <div className='container center'>
               <div className='row col s12 m8 offset-m2'>
+                <div className= 'card blue-grey darken-1'>
+                <div className= 'card-content white-text'>
                 <div id='profile'>
-                  <p>Company: </p>
-                  <p>Phone: </p>
-                  <p>Address: </p>
-                  <p>Services: </p>
+                  <p>Welcome, {this.userFirstName}!  </p>
                 </div>
                 <div id='quotes'>
-                  <p>Pending Requests</p>
-                  <p>Completed Requests</p>
+                  <p><strong>Pending Requests:</strong></p>
+                  <p>{this.userPendingRequests}</p>
+                  <p><strong>Completed Requests:</strong></p>
+                  <p>{this.userCompletedRequests}</p>
                 </div>
               </div>
-            </div>
-          </div>);
+              </div>
+              </div>
+            </div>);
   }
 }
---
+++ app/views/dashboard/request.json.jbuilder
@@ -0,0 +1,5 @@
+json.request do 
+  json.title @request.title
+  json.body @request.body
+  json.phone @request.phone
+end
