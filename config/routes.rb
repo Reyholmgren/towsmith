d2c080fb05142f2eb55bf2031f2f226b0392df1d
Scott Ball <scotttball@gmail.com>
2016-01-28 14:26:48 -0700

    deleted dotfiles

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   root "dashboard#index"
   resources :requests
+
+  post '/location', to: 'location#create'
 end
