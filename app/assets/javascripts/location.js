d3e699481ff954f4d81ea9d49da9da831552ec42
Scott Ball <scotttball@gmail.com>
2016-01-28 09:57:31 -0700

    ajax call for location

--- app/assets/javascripts/location.js
+++ app/assets/javascripts/location.js
@@ -5,22 +5,31 @@ function getLocation() {
     } else {
         x.innerHTML = "Geolocation is not supported by this browser.";
     }
-}
+},
 function showPosition(position) {
     lat = position.coords.latitude;
     lon = position.coords.longitude;
+    storeLocation(lat,lon);
     latlon = new google.maps.LatLng(lat, lon)
-    mapholder = document.getElementById('mapholder')
+    mapholder = document.getElementById('mapholder');
     mapholder.style.height = '250px';
     mapholder.style.width = '500px';
-
     var myOptions = {
     center:latlon,zoom:14,
     mapTypeId:google.maps.MapTypeId.ROADMAP,
     mapTypeControl:false,
     navigationControlOptions:{style:google.maps.NavigationControlStyle.SMALL}
     }
-    
     var map = new google.maps.Map(document.getElementById("mapholder"), myOptions);
-    var marker = new google.maps.Marker({position:latlon,map:map,title:"You are here!"});
-}
+    var marker = new google.maps.Marker({position:latlon,map:map,title:"You are here!";
+},
+function storeLocation(lat,lon) {
+  $.ajax({
+    url: '/location',
+    type: 'POST',
+    data: { lat: lat, lon: lon }
+    success: function() {
+      // TODO: make an html 'Location Stored'
+      }
+    })
+};
